2a588d5c683c84004fb496a9cd843b9cdd503d40
anon123123123 <cditodavis96@gmail.com>
2022-04-02 11:27:04 -0600

    Added auth routing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const budgetRoutes = require('./budgetRoutes');
+const authRoutes = require('./authRoutes');
 
 router.use('/budget', budgetRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
