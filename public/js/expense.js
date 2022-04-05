d8be54fe74a2657b2ad27023cbfdc3bfc980926b
kylejames20 <k.salmons@me.com>
2022-04-05 17:41:47 -0600

    updated expense.js

--- public/js/expense.js
+++ public/js/expense.js
@@ -1,5 +1,16 @@
-async function expenses() {
-    const expenseCategory = document.getElementById('expenses').value
-    const expenseValue = document.getElementById('dollabills').textContent
-}
+const submitExpense = async(event) => {
+    event.preventDefault();
 
+    const nameOfExpense = document.getElementById('nameofexpense').value
+    const expenseCategory = document.getElementById('expensecategory').value
+    const expenseValue = document.getElementById('costofexpense').value
+    const expenseDate = document.getElementById('date').value
+
+    fetch('/api/expense', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({expense_name: nameOfExpense, category: expenseCategory, expense_amount: expenseValue, expense_date: expenseDate})
+    })
+};
+
+document.getElementById("submitexpense").addEventListener("submit", submitExpense);
