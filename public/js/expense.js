d64ba77cc25705e5d562f8ba73e9668b9d6a5511
rainbowdyno <67548773+rainbowdyno@users.noreply.github.com>
2022-04-10 17:58:19 -0600

    edit and delete fully functioning on expenses

--- public/js/expense.js
+++ public/js/expense.js
@@ -22,7 +22,7 @@ const submitExpense = async(event) => {
     expenseCategory.value = "";
     expenseValue.value = "";
     expenseDate.value = "";
-    
+    location.reload()
 };
 
 document.getElementById("submitexpense").addEventListener("submit", submitExpense);
@@ -32,32 +32,3 @@ document.getElementById("submitexpense").addEventListener("submit", submitExpens
 
 //add a new function here that would generate a row under the correct columns to add the info to the webpage when the button is pushed
 
-$(function () {
-  // Get the text for the Handlebars template from the script element.
-  var templateText = $("#tableTemplate").html();
-  
-  // Compile the Handlebars template.
-  var tableTemplate = Handlebars.compile(templateText);
-
-	// Define an array of people.
-  var people = [
-    { "Id": 1, "First Name": "Anthony", "Last Name": "Nelson", "Age": 25 },
-    { "Id": 2, "First Name": "Helen", "Last Name": "Garcia", "Age": 32 },
-    { "Id": 3, "First Name": "John", "Last Name": "Williams", "Age": 48 }
-  ];
-  
-  // Evaluate the template with an array of people and set the HTML
-  // for the people table.
-  $("#people").html(tableTemplate({ array: people }));
-  
-  // Deine an array of smart phones.
-  var smartPhones = [
-  	{ "Manufacturer": "Apple", "Phone": "iPhone", "Operating System": "iOS" },
-    { "Manufacturer": "Samsung", "Phone": "Galaxy", "Operating System": "Android" },
-    { "Manufacturer": "Nokia", "Phone": "Lumia", "Operating System": "Windows" }
-  ];
-  
-  // Evaluate the same table template with an array of smart phoes and set the HTML
-  // for the smartphones table.
-  $("#smartphones").html(tableTemplate({ array: smartPhones }));
-});
--- public/js/getExpenses.js
+++ public/js/getExpenses.js
@@ -8,21 +8,42 @@ class Expense {
     }
 }
 
+async function editExpense() {
+    const expenseId = this.getAttribute('value')
+    const tableRow = document.getElementById('tr' + expenseId)
+    const expense_name = tableRow.children[0].children[0].children[0].value
+    const category = tableRow.children[1].children[0].value
+    const expense_amount = tableRow.children[2].children[0].value
+    const date = new Date (tableRow.children[3].children[0].value)
+    console.log(category)
+    await fetch(`/api/expense/${expenseId}`, {
+        method: 'PUT',
+        body: JSON.stringify({ expense_name, category, expense_amount, date }),
+        headers: { 'Content-Type': 'application/json'},
+    })
+    location.reload()
+}
+
+async function deleteExpense() {
+    const expenseId = this.getAttribute('value')
+    const tableRow = document.getElementById('tr' + expenseId)
+    await fetch(`/api/expense/${expenseId}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json'},
+    })
+    location.reload()
+}
+
 async function expensesOnPageLoad(data, arr) {
 
     const generatedTableRow = document.querySelectorAll('.generated-tr')
-    const generatedTableData = document.querySelectorAll('.generated-td')
     generatedTableRow.forEach(tableRow => {
         tableRow.remove();
       });
-    generatedTableData.forEach(tableData => {
-        tableData.remove()
-    })
 
     for (let i = 0; i < data.length; i++) {
-        console.log('for loop')
         const element = data[i];
-        const newDate = element.expenseDate.split('-')[1] + '/' + element.expenseDate.split('-')[2][0] + element.expenseDate.split('-')[2][1] + '/' + element.expenseDate.split('-')[0] 
+        const newDate = element.expenseDate.split('-')[0] + '-' + element.expenseDate.split('-')[1] + '-' + element.expenseDate.split('-')[2][0] + element.expenseDate.split('-')[2][1]
 
         if (element.housing) {
             arr.push(new Expense(element.name, 'housing', element.housing, newDate, element.id))
@@ -47,30 +68,106 @@ function newRow(param1, param2, param3, param4, param5) {
 
     const table = document.getElementById('expenses-table')
     const tableRow = document.createElement('tr')
+    tableRow.setAttribute('id', 'tr' + param5)
     tableRow.classList.add('generated-tr')
     table.appendChild(tableRow)
+
     const col1 = document.createElement('td')
     col1.classList.add('generated-td')
-    col1.textContent = param1
+    const form1 = document.createElement('form')
+    const input1 = document.createElement('input')
+    input1.setAttribute('value', param1)
     tableRow.appendChild(col1)
+    col1.appendChild(form1)
+    form1.appendChild(input1)
+
+
     const col2 = document.createElement('td')
     col2.classList.add('generated-td')
-    col2.textContent = param2
+    const select = document.createElement('select')
+
+    const option1 = document.createElement('option')
+    option1.setAttribute('value', 'housing')
+    option1.textContent = 'Housing'
+
+    const option2 = document.createElement('option')
+    option2.setAttribute('value', 'insurance')
+    option2.textContent = 'Inusurance'
+
+    const option3 = document.createElement('option')
+    option3.setAttribute('value', 'transportation')
+    option3.textContent = 'Transportation'
+
+    const option4 = document.createElement('option')
+    option4.setAttribute('value', 'food')
+    option4.textContent = 'Food'
+
+    const option5 = document.createElement('option')
+    option5.setAttribute('value', 'savings')
+    option5.textContent = 'Savings'
+
+    const option6 = document.createElement('option')
+    option6.setAttribute('value', 'utilities')
+    option6.textContent = 'Utilities'
+
+    const option7 = document.createElement('option')
+    option7.setAttribute('value', 'personal')
+    option7.textContent = 'Personal'
+
+    switch (param2) {
+        case 'housing':
+        option1.setAttribute('selected', 'selected')
+        break;
+        case 'insurance':
+        option2.setAttribute('selected', 'selected')
+        break;
+        case 'transportation':
+        option3.setAttribute('selected', 'selected')
+        break;
+        case 'food':
+        option4.setAttribute('selected', 'selected')
+        break;
+        case 'savings':
+        option5.setAttribute('selected', 'selected')
+        break;
+        case 'utilities':
+        option6.setAttribute('selected', 'selected')
+        break;
+        case 'personal':
+        option7.setAttribute('selected', 'selected')
+        break;
+    }
     tableRow.appendChild(col2)
+    col2.appendChild(select)
+    select.appendChild(option1)
+    select.appendChild(option2)
+    select.appendChild(option3)
+    select.appendChild(option4)
+    select.appendChild(option5)
+    select.appendChild(option6)
+    select.appendChild(option7)
+
     const col3 = document.createElement('td')
     col3.classList.add('generated-td')
-    col3.textContent = param3
+    const input3 = document.createElement('input')
+    input3.setAttribute('value', param3)
     tableRow.appendChild(col3)
+    col3.appendChild(input3)
+
     const col4 = document.createElement('td')
     col4.classList.add('generated-td')
-    col4.textContent = param4
+    const input4 = document.createElement('input')
+    input4.setAttribute('type', 'date')
+    input4.setAttribute('value', param4)
     tableRow.appendChild(col4)
+    col4.appendChild(input4)
 
     const col5 = document.createElement('td')
     col5.classList.add('generated-td')
     const editButton = document.createElement('button')
     editButton.textContent = 'Edit'
     editButton.setAttribute('value', param5)
+    editButton.addEventListener('click', editExpense)
     tableRow.appendChild(col5)
     col5.appendChild(editButton)
 
@@ -79,6 +176,7 @@ function newRow(param1, param2, param3, param4, param5) {
     const deleteButton = document.createElement('button')
     deleteButton.textContent = 'Delete'
     deleteButton.setAttribute('value', param5)
+    deleteButton.addEventListener('click', deleteExpense)
     tableRow.appendChild(col6)
     col6.appendChild(deleteButton)
 }
@@ -100,12 +198,9 @@ async function getExpenses() {
    return fetchRequest
 }
 
-// getExpenses()
+async function getExpensesByDate() {
 
-async function getExpensesByDate(event) {
-    event.preventDefault()
     let expensesArr =[]
-
     const start = document.getElementById('start-date').value
     const end = document.getElementById('end-date').value
     const startDate = new Date(start)
@@ -125,8 +220,41 @@ async function getExpensesByDate(event) {
        });
    })
    return fetchRequest
+}
+
+async function getExpensesByDateForm(e) {
+    e.preventDefault()
+  
+    let expensesArr =[]
+    const start = document.getElementById('start-date').value
+    const end = document.getElementById('end-date').value
+    const startDate = new Date(start)
+    const endDate = new Date(end)
+
+    const fetchRequest = await fetch(`/api/expense/date/${startDate}/${endDate}`, {
+      headers: { 'Content-Type': 'application/json' },
+    }).then(function (response) {
+        return response.json();
+
+      }).then(function (data) {
+        expensesOnPageLoad(data, expensesArr)
+
+   }).then(function () {
+       expensesArr.forEach(expense => {
+           newRow(expense.name, expense.category, expense.value, expense.date, expense.id)
+       });
+   })
+   return fetchRequest
+}
+
+  document.getElementById('expenses-by-date').addEventListener('submit', getExpensesByDateForm)
+
+let startTime = document.getElementById('start-date').value
+let endTime = document.getElementById('end-date').value
+
+
+  if(startTime && endTime) {
+        getExpensesByDate()
+  } else {
+        getExpenses() 
   }
-
-  document.getElementById('expenses-by-date').addEventListener('submit', getExpensesByDate)
-
-  getExpenses()
