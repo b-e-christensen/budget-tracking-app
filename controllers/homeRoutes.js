cf6ad62cacbbb7e21e123f99c2d944508a839982
kylejames20 <k.salmons@me.com>
2022-04-11 12:31:32 -0600

    added logout button to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const req = require('express/lib/request');
 const { redirect } = require('express/lib/response');
 const { User, Budget, Expense, Income } = require('../models');
 const withAuth = require('../utils/auth');
@@ -14,15 +15,16 @@ router.get('/', withAuth, async (req, res) => {
             res.redirect('/income')
         } else if (user.budget == null) {
             res.redirect('/first-budget')
-        } else {
+        } else {    
         res.render('home', {
             user, logged_in: req.session.logged_in
-        });
+        });    
         }
     } catch (err) {
         res.status(500).json(err)
     }
 })
+
 
 // GET ROUTE FOR PRODUCTION PURPOSES ONLY (easy way to see the breakdown of the data on users being passed)
 router.get('/user/:id', async (req, res) => {
@@ -63,8 +65,8 @@ router.get('/expenses', withAuth, async (req, res) => {
     }
 })
 
-router.get('/budget', (req, res) => {
-    res.render('budget');
+router.get('/budget', withAuth, (req, res) => {
+    res.render('budget', {logged_in: req.session.logged_in});
 })
 
 router.get('/calendar', (req, res) => {
@@ -72,15 +74,15 @@ router.get('/calendar', (req, res) => {
 })
 
 router.get('/income', (req, res) => {
-    res.render('income')
+    res.render('income');
 })
 
 router.get('/first-budget', (req, res) => {
-    res.render('first-budget')
+    res.render('first-budget');
 })
 
 router.get('/news', (req, res) => {
-    res.render('news')
+    res.render('news');
 })
 
 
